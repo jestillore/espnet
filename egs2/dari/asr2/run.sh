e3a41579eb76609b36684c1d40f789b8a2dc72e5
Jillberth Estillore <ejillberth@gmail.com>
2025-07-04 15:59:18 +0200

    Update run.sh

--- egs2/dari/asr2/run.sh
+++ egs2/dari/asr2/run.sh
@@ -3,13 +3,11 @@ set -eou pipefail
 
 ./asr2.sh \
     --lang dari \
-    --feats_type raw \
-    --token_type char \
     --asr_config conf/train_asr_transformer.yaml \
     --inference_config conf/decode_asr.yaml \
     --lm_config conf/train_lm.yaml \
-    --train_set train \
-    --valid_set valid \
-    --test_sets "test" \
+    --train_set data/train \
+    --valid_set data/valid \
+    --test_sets "data/valid data/test" \
     --lm_train_text "data/train/text" \
-    --srctexts "data/train/text" "$@"
+    --speed_perturb_factors "0.9 1.0 1.1" "$@"
