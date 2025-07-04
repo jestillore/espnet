8b0d38ef07a0852b3db4f6e170cdb9fbab22d30b
Jillberth Estillore <ejillberth@gmail.com>
2025-07-04 15:49:53 +0200

    Add the run script

---
+++ egs2/dari/asr2/run.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+set -eou pipefail
+
+./asr2.sh \
+    --lang dari \
+    --feats_type raw \
+    --token_type char \
+    --asr_config conf/train_asr_transformer.yaml \
+    --inference_config conf/decode_asr.yaml \
+    --lm_config conf/train_lm.yaml \
+    --train_set train \
+    --valid_set valid \
+    --test_sets "test" \
+    --lm_train_text "data/train/text" \
+    --srctexts "data/train/text" "$@"
