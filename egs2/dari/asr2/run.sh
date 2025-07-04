60a06fa44a451fc3500f58053bda639a20ee0a9a
Jillberth Estillore <ejillberth@gmail.com>
2025-07-04 16:20:41 +0200

    Generate utt2spk automatically

--- egs2/dari/asr2/run.sh
+++ egs2/dari/asr2/run.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 set -eou pipefail
 
+# Generate spk2utt if missing
+[ ! -f data/train/spk2utt ] && utils/utt2spk_to_spk2utt.pl data/train/utt2spk > data/train/spk2utt
+[ ! -f data/test/spk2utt ] && utils/utt2spk_to_spk2utt.pl data/test/utt2spk > data/test/spk2utt
+[ ! -f data/valid/spk2utt ] && utils/utt2spk_to_spk2utt.pl data/valid/utt2spk > data/valid/spk2utt
+
 ./asr2.sh \
     --lang dari \
     --asr_config conf/train_asr_transformer.yaml \
